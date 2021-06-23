8a39107938e9d2fd33fbdeed16cb2999f473061d
TaylorShakespear <52114607+TaylorShakespear@users.noreply.github.com>
2021-06-23 01:12:29 -0600

    added functionality to check for user input

--- mac-changer.py
+++ mac-changer.py
@@ -3,6 +3,20 @@
 import subprocess
 import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+
+    parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        # code to handle error
+        parser.error("[-] Please specify an interface. Use --help for more info.")
+    elif not options.new_mac:
+        # code to handle error
+        parser.error("[-] Please specify a new MAC. Use --help for more info.")
+    return options
+
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
     # bring interface down
@@ -12,11 +26,5 @@ def change_mac(interface, new_mac):
     # enable mac address
     subprocess.call(["ifconfig", interface, " up"])
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address")
-
-(options, arguments) = parser.parse_args()
-
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
