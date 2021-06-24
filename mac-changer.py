0aae80fdf1eaf497c8c41cf502903ecaf070f4df
TaylorShakespear <52114607+TaylorShakespear@users.noreply.github.com>
2021-06-23 21:52:06 -0600

    cleaned up code

--- mac-changer.py
+++ mac-changer.py
@@ -27,14 +27,23 @@ def change_mac(interface, new_mac):
     # enable mac address
     subprocess.call(["ifconfig", interface, " up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
 options = get_arguments()
-# change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
+change_mac(options.interface, options.new_mac)
 
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-if mac_address_search_result:
-    print(mac_address_search_result.group(0))
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was succesfully changed to " + current_mac)
 else:
-    print("[-] Could not read MAC address.")
+    print("[-] MAC address did not get changed.")
